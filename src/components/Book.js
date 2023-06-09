07390a36c1934cd1b0f7a732c6a4b855f44b5086
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-06-09 18:16:21 +0100

    Add book.js

---
+++ src/components/Book.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BookList from './List';
+import Addform from './Form';
+
+const Book = () => (
+  <>
+    <BookList />
+    <Addform />
+  </>
+);
+export default Book;
