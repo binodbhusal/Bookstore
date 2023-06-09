688f6d3c7c22681791f592fb915fcc5b889d65cc
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-06-09 18:20:16 +0100

    Add Data.js to store bookitem

---
+++ src/components/Data.js
@@ -0,0 +1,18 @@
+const Data = [
+  {
+    id: 1,
+    title: 'The Humger Game',
+    author: 'Suzanne collins',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    author: 'Frank Herbert',
+  },
+  {
+    id: 3,
+    title: 'Capital in the 21st Century',
+    author: 'Suzanne collins',
+  },
+];
+export default Data;
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+const Addform = () => (
+  <>
+    <h2>Add New Book</h2>
+    <form>
+      <input type="text" placeholder="Booktitle" />
+      <input type="text" placeholder="Author" />
+      <button type="submit">Add Book</button>
+    </form>
+  </>
+);
+
+export default Addform;
