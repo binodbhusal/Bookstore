edce7826aa55d2cfc010d2ee4e159bb19440adc9
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-06-14 00:21:15 +0100

    Add book item

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,25 @@
-const Addform = () => (
-  <>
-    <h2>Add New Book</h2>
-    <form>
-      <input type="text" placeholder="Booktitle" />
-      <input type="text" placeholder="Author" />
-      <button type="submit">Add Book</button>
-    </form>
-  </>
-);
+import { useDispatch } from 'react-redux';
+import { addBook } from '../Redux/books/booksSlice';
+
+const Addform = () => {
+  const dispatch = useDispatch();
+
+  const addNewBook = (e) => {
+    e.preventDefault();
+    const title = e.target.elements.bookTitle.value;
+    const author = e.target.elements.author.value;
+    dispatch(addBook({ title, author }));
+  };
+  return (
+    <>
+      <h2>Add New Book</h2>
+      <form onSubmit={addNewBook}>
+        <input type="text" name="bookTitle" placeholder="Booktitle" />
+        <input type="text" name="author" placeholder="Author" />
+        <button type="submit">Add Book</button>
+      </form>
+    </>
+  );
+};
 
 export default Addform;
