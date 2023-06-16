999376d93bedf3ecb037629ff75ad67b83cf52e8
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-06-16 15:55:04 +0100

    Remove from initial state

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,16 +3,19 @@ const Data = [
     id: 1,
     title: 'The Humger Game',
     author: 'Suzanne collins',
+    category: 'Fiction',
   },
   {
     id: 2,
     title: 'Dune',
     author: 'Frank Herbert',
+    category: 'Fiction',
   },
   {
     id: 3,
     title: 'Capital in the 21st Century',
     author: 'Suzanne collins',
+    category: 'Fiction',
   },
 ];
 export default Data;
